988de13ae5310e036f7928068823f2da44af41f6
Jhoset Fabian <lopezrenteriajhosetfabian@gmail.com>
2025-10-17 11:19:59 -0500

    Tercera subida

--- ex01/src/App.jsx
+++ ex01/src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [contar, setContar] = useState(0)
+
+  const [nombre, setNombre] = useState('')
+
+  const manejarClic = () => {
+    setContar(contar + 1)
+  }
+
+  const reiniciarClic = () => {
+    setContar(0)
+  }
+
+  const escucharUser = (e) => {
+    setNombre(e.target.value)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Contador</h1>
+      <p>{contar}</p>
+      <button onClick={manejarClic}>Aumentar</button>
+      <button onClick={reiniciarClic}>Reiniciar</button>
+      <input type="text" placeholder='Escriba su nombre' onChange={escucharUser} />
+      <p>Tu nombre es: {nombre}</p>
     </>
   )
 }
